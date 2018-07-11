445be5e6567d94fdb40e203c201bef97aad7abd4
Uroš Jevremović <jevremovic.uros91@gmail.com>
2018-07-11 23:24:41 +0200

    updated post update view to make it impossible for "x" user to update post, and added test to make sure it works as intented, also added Payment view and mocked stripe to test it is working properly

--- posts/tests/test_views.py
+++ posts/tests/test_views.py
@@ -1,10 +1,13 @@
 from django.contrib.auth.models import AnonymousUser
+from django.core import mail
+from django.http import Http404
 from django.test import RequestFactory
 
 import pytest
 from mixer.backend.django import mixer
+from mock import patch
 
-from posts.views import HomeView, AdminView, PostUpdateView
+from posts.views import HomeView, AdminView, PostUpdateView, PaymentView
 
 pytestmark = pytest.mark.django_db
 
@@ -41,11 +44,31 @@ class TestPostUpdateView:
         assert response.status_code == 200, 'Should be callable by registered users'
 
     def test_post_request(self):
+        user = mixer.blend('auth.User')
         post = mixer.blend('posts.Post')
         data = {'title': 'Title', 'body': 'Body of the message.'}
         req = RequestFactory().post('/', data=data)
+        req.user = user
         response = PostUpdateView.as_view()(req, pk=post.pk)
         assert response.status_code == 302, 'Should redirect to success view'
         post.refresh_from_db()
         assert post.title == 'Title', 'Should update title of the post'
         assert post.body == 'Body of the message.', 'Should update body text of the post'
+
+    def test_security(self):
+        user = mixer.blend('auth.User', first_name='Crash')
+        post = mixer.blend('posts.Post')
+        req = RequestFactory().post('/', data={'title': 'Title', 'body': 'This will fail to submit'})
+        req.user = user
+        with pytest.raises(Http404):
+            PostUpdateView.as_view()(req, pk=post.pk)
+
+
+class TestPaymentView:
+    @patch('posts.views.stripe')
+    def test_payment(self, mock_stripe):
+        mock_stripe.Charge.return_value = {'id': '1234'}
+        req = RequestFactory().post('/', data={'token': '123'})
+        response = PaymentView.as_view()(req)
+        assert response.status_code == 302, 'Should redirect to success url'
+        assert len(mail.outbox) == 1, 'Should send an email'
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,10 @@
+import stripe
 from django.contrib.auth.decorators import login_required
+from django.core.mail import send_mail
+from django.http import Http404
+from django.shortcuts import redirect
 from django.utils.decorators import method_decorator
-from django.views.generic import TemplateView, UpdateView
+from django.views.generic import TemplateView, UpdateView, View
 
 from posts.forms import PostForm
 from posts.models import Post
@@ -23,3 +27,25 @@ class PostUpdateView(UpdateView):
     model = Post
     form_class = PostForm
     success_url = '/'
+
+    def post(self, request, *args, **kwargs):
+        if getattr(request.user, 'first_name', None) == 'Crash':
+            raise Http404()
+        return super().post(request, *args, **kwargs)
+
+
+class PaymentView(View):
+    def post(self, request, *args, **kwargs):
+        charge = stripe.Charge.create(
+            amount=100,
+            currency='usd',
+            description='',
+            token=request.POST.get('token'),
+        )
+        send_mail(
+            'Payment received',
+            'Charge {} succeeded'.format(charge['id']),
+            'test_me@test.com',
+            ['admin@test.com', ],
+        )
+        return redirect('/')
