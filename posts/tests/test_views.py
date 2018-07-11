8d1b6cb25e46abdaeb686288861564d2f10ce203
Uroš Jevremović <jevremovic.uros91@gmail.com>
2018-07-11 23:01:02 +0200

    added view for updating post and tests to make sure both get and post requests are working properly

--- posts/tests/test_views.py
+++ posts/tests/test_views.py
@@ -4,7 +4,7 @@ from django.test import RequestFactory
 import pytest
 from mixer.backend.django import mixer
 
-from posts.views import HomeView, AdminView
+from posts.views import HomeView, AdminView, PostUpdateView
 
 pytestmark = pytest.mark.django_db
 
@@ -29,3 +29,23 @@ class TestAdminView:
         req.user = user
         response = AdminView.as_view()(req)
         assert response.status_code == 200, 'Super user can access all posts'
+
+
+class TestPostUpdateView:
+    def test_get_request(self):
+        user = mixer.blend('auth.User')
+        req = RequestFactory().get('/')
+        req.user = user
+        obj = mixer.blend('posts.Post')
+        response = PostUpdateView.as_view()(req, pk=obj.pk)
+        assert response.status_code == 200, 'Should be callable by registered users'
+
+    def test_post_request(self):
+        post = mixer.blend('posts.Post')
+        data = {'title': 'Title', 'body': 'Body of the message.'}
+        req = RequestFactory().post('/', data=data)
+        response = PostUpdateView.as_view()(req, pk=post.pk)
+        assert response.status_code == 302, 'Should redirect to success view'
+        post.refresh_from_db()
+        assert post.title == 'Title', 'Should update title of the post'
+        assert post.body == 'Body of the message.', 'Should update body text of the post'
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,9 @@
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, UpdateView
+
+from posts.forms import PostForm
+from posts.models import Post
 
 
 class HomeView(TemplateView):
@@ -13,3 +16,10 @@ class AdminView(TemplateView):
     @method_decorator(login_required)
     def dispatch(self, request, *args, **kwargs):
         return super().dispatch(request, *args, **kwargs)
+
+
+class PostUpdateView(UpdateView):
+    template_name = 'posts/update.html'
+    model = Post
+    form_class = PostForm
+    success_url = '/'
